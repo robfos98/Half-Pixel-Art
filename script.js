4c005af7e1eea7572ea6bbcb75da8dd7b4ecb6e5
Robert Foster <robert@Roberts-MBP.lan>
2024-10-08 18:05:33 -0500

    hardcoded drawing

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(() => {
-    const canvas = document.getElementsByName("canvas")[0];
+    const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+    for(let x = 0; x < 10; x++) {
+        for(let y = 0; y < 10; y++) {
+            ctx.moveTo(100 * x, 100 * y);
+            ctx.beginPath();
+            ctx.lineTo(100 * (x + 1), 100 * y);
+            ctx.lineTo(100 * (x + 1), 100 * (y + 1));
+            ctx.fillStyle("brown");
+            ctx.fill();
+            ctx.lineTo(100 * x, 100 * (y + 1));
+            ctx.lineTo(100 * x, 100 * y);
+            ctx.fillStyle("dark brown");
+            ctx.fill();
+        }
+    }
 });
