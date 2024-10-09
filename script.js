2164d8287fb7c417e8ede366c3bf850a67bdf914
Robert Foster <robfos98@gmail.com>
2024-10-09 02:09:35 -0500

    Where does the div show up

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).on('click', 'canvas', function (click) {
     const ctx = $(this).get(0).getContext("2d");
-    var x = Math.floor(click.pageX / 128);
-    var y = Math.floor(click.pageY / 128);
-    var up = click.pageX + click.pageY - 128 * (x + y) <= 128;
+    var x = Math.floor(click.pageX / 16);
+    var y = Math.floor(click.pageY / 16);
+    var up = click.pageX + click.pageY - 16 * (x + y) <= 16;
     if (up) {
-        ctx.moveTo(128 * x + 1, 128 * (y + 1));
+        ctx.moveTo(16 * x + 1, 16 * (y + 1));
         ctx.beginPath();
-        ctx.lineTo(128 * x, 128 * (y + 1));
-        ctx.lineTo(128 * x, 128 * y);
-        ctx.lineTo(128 * (x + 1), 128 * y);
-        ctx.lineTo(128 * (x + 1), 128 * y + 1);
-        ctx.moveTo(128 * x + 1, 128 * (y + 1));
+        ctx.lineTo(16 * x, 16 * (y + 1));
+        ctx.lineTo(16 * x, 16 * y);
+        ctx.lineTo(16 * (x + 1), 16 * y);
+        ctx.lineTo(16 * (x + 1), 16 * y + 1);
+        ctx.moveTo(16 * x + 1, 16 * (y + 1));
     } else {
-        ctx.moveTo(128 * x, 128 * (y + 1));
+        ctx.moveTo(16 * x, 16 * (y + 1));
         ctx.beginPath();
-        ctx.lineTo(128 * (x + 1), 128 * (y+ 1));
-        ctx.lineTo(128 * (x + 1), 128 * y);
-        ctx.lineTo(128 * x, 128 * (y + 1));
+        ctx.lineTo(16 * (x + 1), 16 * (y+ 1));
+        ctx.lineTo(16 * (x + 1), 16 * y);
+        ctx.lineTo(16 * x, 16 * (y + 1));
     }
     ctx.fillStyle = "green";
     ctx.fill();
