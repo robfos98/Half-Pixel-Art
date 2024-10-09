e3421a22079c42f217f5df28536f1be9754c5eab
Robert Foster <robfos98@gmail.com>
2024-10-08 22:30:40 -0500

    combat seams

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ $(document).on('click', 'canvas', function (click) {
     var y = Math.floor(click.pageY / 100);
     var up = click.pageX + click.pageY - 100 * (x + y) <= 100;
     console.log(x + ", " + y + "; " + up);
-    ctx.moveTo(100 * x, 100 * (y + 1));
-    ctx.beginPath();
-    if (up) { ctx.lineTo(100 * x, 100 * y); } else { ctx.lineTo(100 * (x + 1), 100 * (y+ 1)); }
-    ctx.lineTo(100 * (x + 1), 100 * y);
-    ctx.lineTo(100 * x, 100 * (y + 1));
+    if (up) {
+        ctx.moveTo(100 * x, 100 * (y + 1) + 1);
+        ctx.beginPath();
+        ctx.lineTo(100 * x, 100 * y);
+        ctx.lineTo(100 * (x + 1) + 1, 100 * y);
+        ctx.lineTo(100 * x, 100 * (y + 1) + 1);
+    } else {
+        ctx.moveTo(100 * x, 100 * (y + 1));
+        ctx.beginPath();
+        ctx.lineTo(100 * (x + 1), 100 * (y+ 1));
+        ctx.lineTo(100 * (x + 1), 100 * y);
+        ctx.lineTo(100 * x, 100 * (y + 1));
+    }
     ctx.fillStyle = "green";
     ctx.fill();
 });
