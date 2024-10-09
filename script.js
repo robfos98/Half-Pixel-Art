99067073b3b4c46a91a405be8c12a8fdabf43ae2
Robert Foster <robert@Roberts-MBP.lan>
2024-10-08 21:04:41 -0500

    Flipping y

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ $(document).ready(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     for(let x = 0; x < 10; x++) {
-        for(let y = 0; y < 10; y++) {
+        for(let y = 10; y > 0; y--) {
             ctx.moveTo(100 * x, 100 * y);
             ctx.beginPath();
             ctx.lineTo(100 * (x + 1), 100 * y);
-            ctx.lineTo(100 * (x + 1), 100 * (y + 1));
+            ctx.lineTo(100 * (x + 1), 100 * (y - 1));
             ctx.lineTo(100 * x, 100 * y);
             ctx.fillStyle = "red";
             ctx.fill();
 
             ctx.moveTo(100 * x, 100 * y);
             ctx.beginPath();
-            ctx.lineTo(100 * x, 100 * (y + 1));
-            ctx.lineTo(100 * (x + 1), 100 * (y + 1));
+            ctx.lineTo(100 * x, 100 * (y - 1));
+            ctx.lineTo(100 * (x + 1), 100 * (y - 1));
             ctx.lineTo(100 * x, 100 * y);
             ctx.fillStyle = "blue";
             ctx.fill();
