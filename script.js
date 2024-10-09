1bdf3a2e641a37a3c438775b74c6e7460f1854ce
Robert Foster <robert@Roberts-MBP.lan>
2024-10-08 21:16:11 -0500

    Actually better jquery

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).on('click', 'canvas', function () {
-    const ctx = $(this).element.get(0).getContext("2d");
+    const ctx = $(this).get(0).getContext("2d");
     ctx.moveTo(100 * 0, 100 * 1);
     ctx.beginPath();
     ctx.lineTo(100 * (0 + 1), 100 * 1);
