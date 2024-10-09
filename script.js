1413c40def69a8b1f9f97df70f2a8e1ceb94f2df
Robert Foster <robert@Roberts-MBP.lan>
2024-10-08 22:17:09 -0500

    Console log

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).on('click', 'canvas', function (click) {
     var x = Math.floor(click.pageX / 100);
     var y = Math.floor(click.pageY / 100);
     var up = click.pageX + click.pageY - 100 * (x + y) <= 100;
+    console.log(x + ", " + y + "; " + up);
     ctx.moveTo(100 * x, 100 * (y + 1));
     ctx.beginPath();
     if (up) { ctx.lineTo(100 * x, 100 * y); } else { ctx.lineTo(100 * (x + 1), 100 * (y+ 1)); }
