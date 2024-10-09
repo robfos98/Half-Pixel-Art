0a28e30a66129aa1c8d4ac02bc94dd8c764e3539
Robert Foster <robfos98@gmail.com>
2024-10-09 00:07:24 -0500

    divisible by 2

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 $(document).on('click', 'canvas', function (click) {
     const ctx = $(this).get(0).getContext("2d");
-    var x = Math.floor(click.pageX / 100);
-    var y = Math.floor(click.pageY / 100);
-    var up = click.pageX + click.pageY - 100 * (x + y) <= 100;
+    var x = Math.floor(click.pageX / 128);
+    var y = Math.floor(click.pageY / 128);
+    var up = click.pageX + click.pageY - 128 * (x + y) <= 128;
     if (up) {
-        ctx.moveTo(100 * x, 100 * (y + 1) + 1);
+        ctx.moveTo(128 * x + 1, 128 * (y + 1));
         ctx.beginPath();
-        ctx.lineTo(100 * x, 100 * y);
-        ctx.lineTo(100 * (x + 1) + 1, 100 * y);
-        ctx.lineTo(100 * x, 100 * (y + 1) + 1);
+        ctx.lineTo(128 * x, 128 * (y + 1));
+        ctx.lineTo(128 * x, 128 * y);
+        ctx.lineTo(128 * (x + 1), 128 * y);
+        ctx.lineTo(128 * (x + 1), 128 * y + 1);
+        ctx.moveTo(128 * x + 1, 128 * (y + 1));
     } else {
-        ctx.moveTo(100 * x, 100 * (y + 1));
+        ctx.moveTo(128 * x, 128 * (y + 1));
         ctx.beginPath();
-        ctx.lineTo(100 * (x + 1), 100 * (y+ 1));
-        ctx.lineTo(100 * (x + 1), 100 * y);
-        ctx.lineTo(100 * x, 100 * (y + 1));
+        ctx.lineTo(128 * (x + 1), 128 * (y+ 1));
+        ctx.lineTo(128 * (x + 1), 128 * y);
+        ctx.lineTo(128 * x, 128 * (y + 1));
     }
     ctx.fillStyle = "green";
     ctx.fill();
