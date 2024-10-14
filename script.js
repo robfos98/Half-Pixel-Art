0ec3c8e40ea795d4dba677b5baa2bfc996c4c6d5
Robert Foster <robfos98@gmail.com>
2024-10-14 15:36:47 -0500

    Probably can upload image.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-$(document).on('click', 'canvas', function (click) {
+$(document).ready(function() {
+    var imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    function handleImage(e){
+        var reader = new FileReader();
+        reader.onload = function(event){
+            var img = new Image();
+            img.onload = function(){
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img,0,0);
+            }
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);     
+    }
+}).on('click', 'canvas', function (click) {
     const canvas = $(this).get(0);
     const ctx = canvas.getContext("2d");
     var x = Math.floor(click.pageX / 16);
