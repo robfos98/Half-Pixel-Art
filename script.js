e3caf93e0bf316b0673d5d01298c9947d98a4453
robfos98 <57842858+robfos98@users.noreply.github.com>
2024-10-28 16:05:21 -0500

    Hole Patch Patch Patch

--- script.js
+++ script.js
@@ -34,9 +34,9 @@ $(document).ready(function() {
     } else {
         ctx.moveTo(16 * x, 16 * (y + 1));
         ctx.beginPath();
-        ctx.lineTo(16 * x, 16 * (y + 1) - 1);
+        ctx.lineTo(16 * x + 1, 16 * (y + 1) - 1);
         ctx.lineTo(16 * (x + 1), 16 * y);
-        ctx.lineTo(16 * (x + 1), 16 * (y+ 1));
+        ctx.lineTo(16 * (x + 1), 16 * (y + 1));
         ctx.lineTo(16 * x, 16 * (y + 1));
     }
     ctx.globalAlpha = document.getElementById("transparency").value / 255;
